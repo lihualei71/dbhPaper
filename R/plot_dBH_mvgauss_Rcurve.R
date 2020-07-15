c28248b18304897c4859b87875737cdae00e1105
lihualei71 <lihualei@stanford.edu>
2020-07-15 00:27:11 -0700

    Finish README

--- R/plot_dBH_mvgauss_Rcurve.R
+++ R/plot_dBH_mvgauss_Rcurve.R
@@ -1,6 +1,7 @@
 source("plot_Rcurve.R")
 
 load("../data/dBH_mvgauss_Rcurve.RData")
+load("../data/dBH_mvgauss_safe_Rcurve.RData")
 
 pdf("../figs/calibrate_gamma1.pdf", width = 5, height = 5)
 plot_Rcurve(Rcurves$BH,
@@ -13,6 +14,23 @@ plot_Rcurve(Rcurves$BH,
             cols = c("red", "blue", "orange"),
             ltys = c(1, 2, 4),
             xlim = c(0, 40 * Rcurves$alpha / n),
+            ylim = c(0.02, 0.05),
+            labels = c(expression("BH"(alpha)),
+                       expression("dBH"[1](alpha)),
+                       expression("dBH"[1]^2*(alpha))))
+dev.off()
+
+pdf("../figs/calibrate_safe.pdf", width = 5, height = 5)
+plot_Rcurve(Rcurves_safe$BH,
+            Rcurves_safe$dBH,
+            Rcurves_safe$dBH2,
+            n = Rcurves_safe$n,
+            side = Rcurves_safe$side,
+            alpha = Rcurves_safe$alpha,
+            avals = Rcurves_safe$avals,
+            cols = c("red", "blue", "orange"),
+            ltys = c(1, 2, 4),
+            xlim = c(0, 40 * Rcurves_safe$alpha / n),
             ylim = c(0.02, 0.05),
             labels = c(expression("BH"(alpha)),
                        expression("dBH"[1](alpha)),
